f3ce521fc18b1318d497ff86a5d3093ed2ce1052
Daniil <oleinik.d_78@mail.ru>
2023-07-09 13:23:34 +0400

    Task_64 done

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int NaturalToN(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    System.Console.Write($"{n} ");
+    return NaturalToN (n-1);
+}
+
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalToN(n);
